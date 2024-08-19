dea665ecf632f5a181733f7c0b2d347a6a750299
vitorbelluzzo[ <vitorbelluzzo@hotmail.com>
2024-08-19 18:20:58 -0300

    feat: Add SmartTv class with methods for controlling TV functions

---
+++ javaBasico/src/CursosintaxeJava/modulos/metodos/SmartTv.java
@@ -0,0 +1,53 @@
+package CursosintaxeJava.modulos.metodos;
+
+public class SmartTv {
+   boolean ligada = false;
+   int canal = 1;
+   int volume = 25;
+
+   // crie o metodo ligar para alterar o atributo ligada
+
+   public void ligarTV() {
+      ligada = true;
+      System.out.println("TV ligada");
+   }
+   // faça o mesmo metodo para desligar
+
+   public void desligarTv() {
+      ligada = false;
+      System.out.println("TV desligada");
+   }
+
+   // crie o metodo aumentarVolume sem parametro que nao retorna nada,
+
+   public void aumentarVolume() {
+      volume++;
+      System.out.println(volume);
+   }
+
+   // faça o mesmo para diminuir volume;
+
+   public void diminuirVolume() {
+      volume--;
+      System.out.println(volume);
+   }
+
+   // crie um semelhante ao de cima para aumentarCanal e DiminuirCanal
+
+   public void aumentarCanal() {
+      canal++;
+      System.out.println(canal);
+   }
+
+   public void diminuirCanal() {
+      canal--;
+      System.out.println(canal);
+   }
+   // crie um metodo para mudar canal esse ira receber um parametro int novoCanal
+
+   public void mudarCanal(int novoCanal) {
+      canal = novoCanal;
+      System.out.println(canal);
+   }
+
+}
---
+++ javaBasico/src/CursosintaxeJava/modulos/metodos/User.java
@@ -0,0 +1,33 @@
+package CursosintaxeJava.modulos.metodos;
+
+public class User {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        // System.out.println("TV ligada: " + smartTv.ligada);
+
+        System.out.println("TV ligada: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.ligarTV();
+
+        System.out.println("Canal alterado para:");
+        smartTv.aumentarCanal();
+
+        System.out.println("Canal alterado para:");
+        smartTv.diminuirCanal();
+
+        System.out.println("Volume alterado para:");
+        smartTv.aumentarVolume();
+
+        System.out.println("volume alterado para:");
+        smartTv.diminuirVolume();
+
+        System.out.println("Canal alterado para:");
+        smartTv.mudarCanal(3);
+
+        smartTv.desligarTv();
+
+    }
+}
