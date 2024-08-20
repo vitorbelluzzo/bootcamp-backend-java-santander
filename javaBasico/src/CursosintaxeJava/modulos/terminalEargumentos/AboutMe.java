1cf53cd91180dcafac55da28f4077b9bceb4d7f2
vitorbelluzzo[ <vitorbelluzzo@hotmail.com>
2024-08-20 17:14:07 -0300

    chore: Update launch.json with new configurations for Java classes in terminalEargumentos module

---
+++ javaBasico/src/CursosintaxeJava/modulos/terminalEargumentos/AboutMe.java
@@ -0,0 +1,28 @@
+package CursosintaxeJava.modulos.terminalEargumentos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+
+   public static void main(String[] args) {
+
+      Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+      System.out.println("Digite seu nome: ");
+      String nome = scanner.next();
+
+      System.out.println("Digite seu Sobrenome: ");
+      String sobrenome = scanner.next();
+
+      System.out.println("Digite sua idade: ");
+      int idade = scanner.nextInt();
+
+      System.out.println("Digite sua altura: ");
+      double altura = scanner.nextDouble();
+
+      System.out.println("Olá, me chamo " + nome + " " + sobrenome + ".");
+      System.out.println("Tenho " + idade + " anos.");
+      System.out.println("Tenho " + altura + " de altura.");
+   }
+}
